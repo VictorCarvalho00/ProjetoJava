1dbeae50b735e6562f92f69a901fa36c8ecd9196
Victor Gabriel <e008732@tst.jus.br>
2025-05-15 11:06:59 -0300

    Inicio da API

--- src/br/com/alura/OrientacaoAObjetos/Principal/PrincipalComBusca.java
+++ src/br/com/alura/OrientacaoAObjetos/Principal/PrincipalComBusca.java
@@ -1,10 +1,20 @@
 package br.com.alura.OrientacaoAObjetos.Principal;
 
+import br.com.alura.OrientacaoAObjetos.modelos.TituiloOmdb;
+import br.com.alura.OrientacaoAObjetos.modelos.Titulo;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.security.cert.X509Certificate;
 import java.util.Scanner;
 
 public class PrincipalComBusca {
@@ -13,14 +23,64 @@ public class PrincipalComBusca {
         System.out.println("Digite um filme para busca: ");
         var busca = leitura.nextLine();
 
-        String endereco = "https://www.omdbapi.com/?i=" + busca + "&apikey=667166cd";
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=667166cd";
+        try {
+        TrustManager[] trustAllCerts = new TrustManager[]{
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return new X509Certificate[0];
+                    }
 
-        HttpClient client = HttpClient.newHttpClient();
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {}
+
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+                }
+        };
+
+        SSLContext sslContext = null;
+        try {
+            sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+        } catch (Exception e) {
+            System.out.println("Erro ao configurar SSL:");
+            e.printStackTrace();
+            return;
+        }
+
+        HttpClient client = HttpClient.newBuilder()
+                .sslContext(sslContext)
+                .build();
+
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+
+//        HttpClient client = HttpClient.newHttpClient();
+//        HttpRequest request = HttpRequest.newBuilder()
+//                .uri(URI.create(endereco))
+//                .build();
+//        HttpResponse<String> response = client
+//                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy
+                        .UPPER_CAMEL_CASE).create();
+        TituiloOmdb meuTituloOmdb = gson.fromJson(json, TituiloOmdb.class);
+        System.out.println(meuTituloOmdb);
+            Titulo meuTitulo = new Titulo(meuTituloOmdb);
+            System.out.println("Titulo convertido");
+            System.out.println(meuTitulo);
+        } catch (NumberFormatException e){
+            System.out.println("Aconteceu um erro: ");
+            System.out.println(e.getMessage());
+        } catch (IllegalArgumentException e){
+            System.out.println("Algum erro de argumento na busca, verifique o endereço");
+        }
+        System.out.println("O programa finalizou!");
     }
 }
---
+++ src/br/com/alura/OrientacaoAObjetos/modelos/TituiloOmdb.java
@@ -0,0 +1,4 @@
+package br.com.alura.OrientacaoAObjetos.modelos;
+
+public record TituiloOmdb(String title, String year, String runtime) {
+}
